1c9903fdf8c78738b27db8a376f1855f086d1493
DavidAnt <davidantarenas@icloud.com>
2024-10-02 01:32:34 -0600

    Actualización de configuración de MySQL para Railway

--- src/db.js
+++ src/db.js
@@ -1,15 +1,15 @@
 import { createPool } from 'mysql2/promise';
 import dotenv from 'dotenv';
 
-// Cargar las variables de entorno desde el archivo .env
+// Cargar las variables de entorno
 dotenv.config();
 
 export const pool = createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
