87eafaa261350fa759a2d6a70469f80b972aad6b
DavidAnt <davidantarenas@icloud.com>
2024-10-02 06:05:48 -0600

    Edicion a tabla

--- src/routes/createTable.routes.js
+++ src/routes/createTable.routes.js
@@ -5,22 +5,13 @@ const router = Router();
 
 router.get('/create-tables', async (req, res) => {
     try {
-
-        // Crear la tabla de subprocesos con una relación a la tabla de procesos
-        const createSubprocesosTable = `
-            CREATE TABLE IF NOT EXISTS subprocesos (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                nombre VARCHAR(255),
-                descripcion VARCHAR(255),
-                proceso_id INT,  -- Llave foránea que referencia a la tabla procesos
-                valor_referencia DECIMAL(5, 2),  -- Valor de referencia con 2 decimales
-                incertidumbre_patron DECIMAL(7, 4),  -- Incertidumbre patrón con 4 decimales
-                FOREIGN KEY (proceso_id) REFERENCES procesos(id) ON DELETE CASCADE
-            );
+        // Crear la tabla de procesos
+        const alterProcesosTable = `
+            ALTER TABLE procesos
+            MODIFY COLUMN resolucion VARCHAR(255);
         `;
-
-        // Ejecutar la consulta para crear las tablas
-        await pool.query(createSubprocesosTable);
+        // Ejecutar las consultas para crear las tablas
+        await pool.query(alterProcesosTable);
 
         res.status(200).json({ message: "Tablas 'procesos' y 'subprocesos' creadas o ya existentes" });
     } catch (error) {
