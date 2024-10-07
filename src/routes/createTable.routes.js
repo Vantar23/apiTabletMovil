034bf54f35dabb472c4d61a680561099c69f20bc
DavidAnt <davidantarenas@icloud.com>
2024-10-07 16:07:14 -0600

    Crea cadena

--- src/routes/createTable.routes.js
+++ src/routes/createTable.routes.js
@@ -3,8 +3,8 @@ import { pool } from '../db.js';  // Asegúrate de que la conexión a la base de
 
 const router = Router();
 
-// Limpiar las tablas procesos, subprocesos y sensores, creando una cadena antes de eliminarlos
-router.get('/clean-database', async (req, res) => {
+// Crear una cadena con los datos de las tablas procesos, subprocesos y sensores sin borrar nada
+router.get('/crea-cadena', async (req, res) => {
     try {
         // Obtener todos los datos de procesos
         const [procesos] = await pool.query('SELECT * FROM procesos');
@@ -33,17 +33,12 @@ router.get('/clean-database', async (req, res) => {
             cadena += `!${sensor.id},${sensor.nombre_sensor || ''},${sensor.mac_address || ''},${sensor.instrumento || ''},${sensor.marca || ''},${sensor.modelo || ''},${sensor.serie || ''},${sensor.resolucion || ''},${sensor.intervalo_indicacion || ''},${sensor.emp || ''},${sensor.temp_inicial || ''},${sensor.temp_final || ''},${sensor.humedad_relativa_inicial || ''},${sensor.humedad_relativa_final || ''},${sensor.presion_atmosferica || ''},${sensor.numero_informe || ''},`;
         });
 
-        // Eliminar los registros después de haber construido la cadena
-        await pool.query('DELETE FROM sensores');
-        await pool.query('DELETE FROM subprocesos');
-        await pool.query('DELETE FROM procesos');
-
-        // Enviar la respuesta con la cadena construida y mensaje de éxito
-        res.status(200).json({ message: 'Las tablas procesos, subprocesos y sensores han sido limpiadas con éxito.', cadena });
+        // Enviar la cadena construida sin eliminar los registros
+        res.status(200).json({ cadena });
 
     } catch (error) {
-        console.error('Error al limpiar las tablas:', error);
-        res.status(500).json({ message: 'Error al limpiar las tablas', error });
+        console.error('Error al crear la cadena:', error);
+        res.status(500).json({ message: 'Error al crear la cadena', error });
     }
 });
 
