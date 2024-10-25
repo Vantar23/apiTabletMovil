3c491615d1ef7fc69046fb8beb30b4b586efbbaa
TuNombre <tuemail@example.com>
2024-10-25 16:16:50 -0600

    .

--- src/routes/createTable.routes.js
+++ src/routes/createTable.routes.js
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-const qs = require('qs');
 import { pool } from '../db.js';  // Asegúrate de que la conexión a la base de datos esté correctamente exportada
 import axios from 'axios'; // Importar axios para hacer la solicitud POST
 
@@ -31,36 +30,18 @@ router.get('/crea-cadena', async (req, res) => {
         });
 
         // Agregar sensores a la cadena
-        sensores.forEach((sensor, index) => {
-            // Mantener el mismo formato, usar un consecutivo a partir de 1
-            const consecutivo = index + 1;
-            cadena += `!${consecutivo},${sensor.nombre_sensor || ''},${sensor.mac_address || ''},${sensor.instrumento || ''},${sensor.marca || ''},${sensor.modelo || ''},${sensor.resolucion || ''},${sensor.intervalo_indicacion || ''},${sensor.emp || ''},`;
+        sensores.forEach(sensor => {
+            cadena += `!${sensor.id},${sensor.nombre_sensor || ''},${sensor.mac_address || ''},${sensor.instrumento || ''},${sensor.marca || ''},${sensor.modelo || ''},${sensor.resolucion || ''},${sensor.intervalo_indicacion || ''},${sensor.emp || ''},`;
         });
 
-        // Enviar la cadena a la URL especificada mediante POST con formato URL encoded
-        const response = await axios.post('https://controlware.com.mx/recibe_avimex_tablet.asp', 
-            qs.stringify({ cadena: cadena }),
-            {
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                }
-            }
-        );
-
-        // Manejar la respuesta de la solicitud
-        if (response.status === 200) {
-            res.status(200).json({ message: 'Cadena enviada con éxito', cadena, response: response.data });
-        } else {
-            res.status(response.status).json({ message: 'Error al enviar la cadena', response: response.data });
-        }
+        // Mostrar la cadena en la respuesta
+        res.status(200).json({ message: 'Cadena creada con éxito', cadena });
 
     } catch (error) {
-        console.error('Error al crear y enviar la cadena:', error);
-        res.status(500).json({ message: 'Error al crear y enviar la cadena', error });
+        console.error('Error al crear la cadena:', error);
+        res.status(500).json({ message: 'Error al crear la cadena', error });
     }
 });
-
-
 
 // Limpiar las tablas procesos, subprocesos y sensores después de crear la cadena
 router.get('/clean-database', async (req, res) => {
