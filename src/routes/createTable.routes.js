84dd4f3f338861d0805483a4b3c82a4cf112c710
TuNombre <tuemail@example.com>
2024-10-25 15:03:13 -0600

    .

--- src/routes/createTable.routes.js
+++ src/routes/createTable.routes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+const qs = require('qs');
 import { pool } from '../db.js';  // Asegúrate de que la conexión a la base de datos esté correctamente exportada
 import axios from 'axios'; // Importar axios para hacer la solicitud POST
 
@@ -36,17 +37,26 @@ router.get('/crea-cadena', async (req, res) => {
             cadena += `!${consecutivo},${sensor.nombre_sensor || ''},${sensor.mac_address || ''},${sensor.instrumento || ''},${sensor.marca || ''},${sensor.modelo || ''},${sensor.resolucion || ''},${sensor.intervalo_indicacion || ''},${sensor.emp || ''},`;
         });
 
-        // Asegurarse de que la cadena esté correctamente codificada para enviar en la URL
-        const url = `https://controlware.com.mx/recibe_avimex_tablet.asp?recibo=${encodeURIComponent(cadena)}`;
+        // Enviar la cadena a la URL especificada mediante POST con formato URL encoded
+        const response = await axios.post('https://controlware.com.mx/recibe_avimex_tablet.asp', 
+            qs.stringify({ cadena: cadena }),
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }
+        );
 
-        // Enviar la cadena mediante una solicitud POST
-        await axios.post(url);
-
-        res.status(200).json({ message: 'Cadena enviada con éxito', cadena });
+        // Manejar la respuesta de la solicitud
+        if (response.status === 200) {
+            res.status(200).json({ message: 'Cadena enviada con éxito', cadena, response: response.data });
+        } else {
+            res.status(response.status).json({ message: 'Error al enviar la cadena', response: response.data });
+        }
 
     } catch (error) {
         console.error('Error al crear y enviar la cadena:', error);
-        res.status(500).json({ message: 'Error al crear y enviar la cadena', error, cadena });
+        res.status(500).json({ message: 'Error al crear y enviar la cadena', error });
     }
 });
 
